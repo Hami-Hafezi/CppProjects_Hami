f7c2a35b8a5954171c862c01ca40b4b45fe581af
Hami-Hafezi <dev.HamiHafezi@gmail.com>
2024-03-03 13:44:19 +0330

    Example 1 : Power

---
+++ BasicsBeforeStatements/1-Example_NumberPower2_1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int number_get;
+  cin >> number_get;
+  number_get = number_get * number_get;
+  cout << number_get;
+  return 0;
+}
--- BasicsBeforeStatements/FIrstProgram.cpp
+++ BasicsBeforeStatements/FIrstProgram.cpp
@@ -1,1 +1,6 @@
 #include <iostream>
+using namespace std;
+int main() {
+  cout << "Hami Starts Programming in 3/2/2024 seriously";
+  return 0;
+}

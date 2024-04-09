4d2d64955aa4ae351424d0d257b84639d9e06a15
Hami-Hafezi <dev.HamiHafezi@gmail.com>
2024-04-09 17:30:36 +0330

    Code Project For "Get number or string and print type of them using switch"

--- If_forWhile_fun/CheckChar.cpp
+++ If_forWhile_fun/CheckChar.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 int main() {
-  
+  char xGetFromUser;
+  cout << "tell me a single string" << endl;
+  cin >> xGetFromUser;
+
+  switch (xGetFromUser) {
+  case 'a':
+    cout << "its a part of alphbaet";
+    break;
+    case '1':
+     cout << "its a part of numbers";
+    break;
+  }
+
   return 0;
 }
